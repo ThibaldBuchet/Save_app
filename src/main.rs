26a172d87728f36be527baabd0a4bc3cf5d27911
Thibald Buchet <thibald.buchet@jobtrek.ch>
2024-06-24 13:24:17 +0200

    Save_app - write and read the file

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,36 @@
+use std::fs::OpenOptions;
+use std::io::{self, Write};
 use std::fs::File;
-use std::io::{BufWriter, Write};
+use std::io::{BufRead};
+use std::path::Path;
+fn main() {
+    println!("Enter the sentence to save or press 'Enter' to see what is already saved.");
 
-fn main() -> std::io::Result<()> {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed to read line");
 
-    let file = File::create("Données_sauvegardées.txt")?;
+    let path = "Données_sauvegardées.txt";
 
-    let mut writer = BufWriter::new(file);
+    let mut file = match OpenOptions::new().append(true).create(true).open(&path) {
+        Err(why) => panic!("Can not open the file: {}", why),
+        Ok(file) => file,
+    };
 
-    writer.write_all(b"Hello! My name is Thibald")?;
+    match file.write_all(input.as_bytes()) {
+        Err(why) => panic!("Can not write in the file: {}", why),
+        Ok(_) => println!("Sentence is save in 'Données_sauvegardées.txt'"),
+    }
 
-    writer.flush()?;
-
-    println!("Write Operation Successful");
-    Ok(())
+    if let Ok(lines) = read_lines("Données_sauvegardées.txt") {
+        for line in lines.flatten() {
+            println!("{}", line);
+        }
+    }
 }
 
-    
+
+fn read_lines<P>(Données_sauvegardées: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open("Données_sauvegardées.txt")?;
+    Ok(io::BufReader::new(file).lines())
+}
