78e152941a1e505881416cb15839cb4dd4440535
Thibald Buchet <thibald.buchet@jobtrek.ch>
2024-06-24 16:43:48 +0200

    Save_app - Remove sentences

--- src/main.rs
+++ src/main.rs
@@ -11,29 +11,30 @@ fn main() {
         println!("----Choose an option:----");
         println!("1: Enter a new sentence");
         println!("2: See file content");
-        println!("3: leave");
+        println!("3: Remove a sentence");
+        println!("4: Leave");
 
         let mut choice = String::new();
         io::stdin()
             .read_line(&mut choice)
-            .expect("Failed to read line");
+            .expect("!Failed to read line!");
 
         match choice.trim() {
             "1" => {
-                println!("Enter the sentense to save:");
+                println!("----Enter the sentence to save:----");
                 let mut input = String::new();
                 io::stdin()
                     .read_line(&mut input)
-                    .expect("Failed to read line");
+                    .expect("!Failed to read line!");
 
                 let mut file = match OpenOptions::new().append(true).create(true).open(&path) {
-                    Err(why) => panic!("Can not open the file: {}", why),
+                    Err(why) => panic!("!Cannot open the file: {}!", why),
                     Ok(file) => file,
                 };
 
                 match file.write_all(input.as_bytes()) {
-                    Err(why) => panic!("Can not write in the file: {}", why),
-                    Ok(_) => println!("Sentence saved in 'Données_sauvegardées.txt'"),
+                    Err(why) => panic!("!Cannot write to the file: {}!", why),
+                    Ok(_) => println!("----Sentence saved in 'Données_sauvegardées.txt'----"),
                 }
             }
             "2" => {
@@ -45,11 +46,42 @@ fn main() {
                 }
             }
             "3" => {
-                println!("Bye!");
+                println!("----Remove a sentence----");
+                if let Ok(mut sentences) = read_sentences(&path) {
+                    for (index, sentence) in sentences.iter().enumerate() {
+                        println!("{}: {}", index, sentence);
+                    }
+
+                    println!("----Enter the index of the sentence to remove:----");
+                    let mut index_str = String::new();
+                    io::stdin()
+                        .read_line(&mut index_str)
+                        .expect("!Failed to read line!");
+                    let index: usize = match index_str.trim().parse() {
+                        Ok(num) => num,
+                        Err(_) => {
+                            println!("!Invalid index!");
+                            continue;
+                        }
+                    };
+
+                    if index < sentences.len() {
+                        sentences.remove(index);
+                        if let Err(why) = write_sentences(&path, &sentences) {
+                            panic!("!Cannot write to the file: {}!", why);
+                        }
+                        println!("----Sentence removed successfully----");
+                    } else {
+                        println!("!Invalid index!");
+                    }
+                }
+            }
+            "4" => {
+                println!("Bye!<3");
                 break;
             }
             _ => {
-                println!("Invalid option, please try again.");
+                println!("!Invalid option, please try again.!");
             }
         }
     }
@@ -61,4 +93,28 @@ where
 {
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
+}
+
+fn read_sentences<P>(filename: P) -> io::Result<Vec<String>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    let reader = io::BufReader::new(file);
+    let mut sentences = Vec::new();
+    for line in reader.lines() {
+        sentences.push(line?);
+    }
+    Ok(sentences)
+}
+
+fn write_sentences<P>(filename: P, sentences: &[String]) -> io::Result<()>
+where
+    P: AsRef<Path>,
+{
+    let mut file = File::create(filename)?;
+    for sentence in sentences {
+        writeln!(file, "{}", sentence)?;
+    }
+    Ok(())
 }
