6f093d4f4655b739079b9a31e449a3895cc10cc7
Thibald Buchet <thibald.buchet@jobtrek.ch>
2024-06-24 10:57:54 +0200

    Save_app - Buffwriter

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    
+use std::fs::File;
+use std::io::{BufWriter, Write};
+
+fn main() -> std::io::Result<()> {
+
+    let file = File::create("Données_sauvegardées.txt")?;
+
+    let mut writer = BufWriter::new(file);
+
+    writer.write_all(b"Hello! My name is Thibald")?;
+
+    writer.flush()?;
+
+    println!("Write Operation Successful");
+    Ok(())
 }
+
+    
